0375f0f19bfd0ca590d4a79c3ad94b32492ba7c5
jacques-serfontein <jben.serfontein@gmail.com>
2023-08-23 17:33:46 +1000

    Added funcitoning Torch version of Q1

--- Q1_pytorch.py
+++ Q1_pytorch.py
@@ -1,9 +1,13 @@
+import torch
 from sklearn.datasets import fetch_lfw_people
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
 import matplotlib.pyplot as plt
 from sklearn.ensemble import RandomForestClassifier
 import numpy as np
+
+# Device configuration
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 # Download the data, if not already on disk and load it as numpy arrays
 lfw_people = fetch_lfw_people(min_faces_per_person=70, resize=0.4)
@@ -19,26 +23,29 @@ n_features = X.shape[1]
 # the label to predict is the id of the person
 y = lfw_people.target
 target_names = lfw_people.target_names
-n_classes = target_names.shape[0]
-# print("Total dataset size:")
-# print("n_samples: %d" % n_samples)
-# print("n_features: %d" % n_features)
-# print("n_classes: %d" % n_classes)
 
 # Split into a training set and a test set using a stratified k fold
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=42)
+
+# Convert to PyTorch tensors
+X_train_tensor = torch.tensor(X_train)
+X_test_tensor = torch.tensor(X_test)
+
+# Send to GPU
+X_train_tensor = X_train_tensor.to(device)
+X_test_tensor = X_test_tensor.to(device)
 
 # Compute a PCA (eigenfaces) on the face dataset (treated as unlabeled
 # dataset): unsupervised feature extraction / dimensionality reduction
 n_components = 150
 
 # Center data
-mean = np.mean(X_train, axis=0)
-X_train -= mean
-X_test -= mean
+mean = torch.mean(X_train_tensor, dim=0)
+X_train_tensor -= mean
+X_test_tensor -= mean
 
 # Eigen-decomposition
-U, S, V = np.linalg.svd(X_train, full_matrices=False)
+U, S, V = torch.linalg.svd(X_train_tensor, full_matrices=False)
 # print(U.shape)
 # print(S.shape)
 # print(V.shape)
@@ -46,9 +53,9 @@ components = V[:n_components]
 eigenfaces = components.reshape((n_components, h, w))
 
 # Project into PCA subspace
-X_transformed = np.dot(X_train, components.T)
-# print(X_transformed.shape)
-X_test_transformed = np.dot(X_test, components.T)
+X_train_transformed = torch.tensordot(X_train_tensor, components.T, dims=1)
+# print(X_train_transformed.shape)
+X_test_transformed = torch.tensordot(X_test_tensor, components.T, dims=1)
 # print(X_test_transformed.shape)
 
 # Qualitative evaluation of the predictions using matplotlib
@@ -63,6 +70,12 @@ def plot_gallery(images, titles, h, w, n_row=3, n_col=4):
         plt.xticks(())
         plt.yticks(())
 
+# Send back to CPU for further calcs
+eigenfaces = eigenfaces.to('cpu').numpy()
+X_train_transformed = X_train_transformed.to('cpu').numpy()
+X_test_transformed = X_test_transformed.to('cpu').numpy()
+S = S.to('cpu').numpy()
+
 eigenface_titles = ["eigenface %d" % i for i in range(eigenfaces.shape[0])]
 plot_gallery(eigenfaces, eigenface_titles, h, w)
 
@@ -73,14 +86,16 @@ total_var = explained_variance.sum()
 explained_variance_ratio = explained_variance / total_var
 ratio_cumsum = np.cumsum(explained_variance_ratio)
 # print(ratio_cumsum.shape)
+
 eigenvalueCount = np.arange(n_components)
 plt.plot(eigenvalueCount, ratio_cumsum[:n_components])
 plt.title('Compactness')
+
 # plt.show()
 
 #build random forest
 estimator = RandomForestClassifier(n_estimators=150, max_depth=15, max_features=150)
-estimator.fit(X_transformed, y_train) #expects X as [n_samples, n_features]
+estimator.fit(X_train_transformed, y_train) #expects X as [n_samples, n_features]
 
 predictions = estimator.predict(X_test_transformed)
 correct = predictions==y_test
